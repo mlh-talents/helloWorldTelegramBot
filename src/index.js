c3d49b319dcbe3471314f36467ef8ab59e278358
Jan Bucher <jan@endlesscoderz.ch>
2016-12-03 12:24:43 +0100

    Add basic index file and ignore all secret files
    the .secret file contains the telegram token

---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log("Hello to the helloworld telegram bot module");
+
+var TelegramBot = require('node-telegram-bot-api');
+var telegramTokenReader = require('fs');
+
+// load token from file
+var token = telegramTokenReader.readFileSync('../telegram_token.secret');
+
+// Create a bot that uses 'polling' to fetch new updates
+var bot = new TelegramBot(token, { polling: true });
+
+// respond on "/echo"
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  var chatId = msg.chat.id;
+  var resp = "Hello world from MLH event :)";
+
+  bot.sendMessage(chatId, resp);
+});
